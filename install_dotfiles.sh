ab84f08ec57edc399d372ccd096830f5f62f726e
g0ods1gnal <g0ods1gnal@proton.me>
2024-10-20 12:51:50 +0100

    alacritty conf

---
+++ install_dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASE=/home/$USER
+
+# Alacritty
+ln -sf alacritty/alacritt.toml $BASE/.config/alacritty/alacritty.toml
+# Dunst
+#ln -sf dunst/dunstrc $BASE/.config/dunst/dunstrc
+# git
+#ln -sf git/gitconfig $BASE/.gitconfig
+# i3
+#ln -sf i3/config $BASE/.config/i3/config
+# i3status-rust
+#ln -sf i3-status-rust/config.toml $BASE/.config/i3status-rust/config.toml
+# picom
+#ln -sf picom/picom.conf $BASE/.config/picom.conf
+# shell
+#ln -sf shell/zshrc $BASE/.zshrc
+# tmux
+#ln -sf tmux/tmux.conf $BASE/.tmux.conf
+# wallpapers
+#ln -sf wallpapers/gentoo.jpg $BASE/.config/wallpaper.jpg
+# xorg
+#ln -sf xorg/xinitrc $BASE/.xinitrc
